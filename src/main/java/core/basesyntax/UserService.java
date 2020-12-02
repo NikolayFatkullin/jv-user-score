b2062f112bcc63dabe6ce913cf8a825b6cecfc67
nikelok01@gmail.com <163amefeZ>
2020-12-02 20:23:23 +0200

    - created package exception; - added class UserNotFoundException to the package exception; - Caught exception in method getUserScore; - built functional of method getUserScore

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,17 @@
 package core.basesyntax;
 
+import core.basesyntax.exception.UserNotFoundException;
+
 public class UserService {
 
     public int getUserScore(String[] records, String email) {
-        return 0;
+
+        for (String userRecords : records) {
+            String[] emailRecords = userRecords.split(":");
+            if (emailRecords[0].equals(email)) {
+                return Integer.parseInt(emailRecords[1]);
+            }
+        }
+        throw new UserNotFoundException("User with given email doesn't exist");
     }
 }
